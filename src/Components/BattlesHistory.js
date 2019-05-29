829d5790011a0ce52ac64be46b4b467f9115618e
Guzeloff <46246043+Guzeloff@users.noreply.github.com>
2019-05-29 02:10:14 +0200

    3rd View added

--- src/Components/BattlesHistory.js
+++ src/Components/BattlesHistory.js
@@ -1,19 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  Link  from 'react-router-dom';
-
-
+import  {Link}  from 'react-router-dom';
 
 class BattleHistory extends React.Component {
         constructor(){
             super()
+            this.state={
+                battles : [
+                {id:1 ,  winteam: "Avengers" },
+                {id:2 ,  winteam: "Villains"  }
+                    ]
         }
+        }
+
+        /*  componentDidMount(){
+        var auth = 'Bearer ' + 'H4OUJ19yD5kKFvE31mkAhG0SJocdtqBQ'
+        var url = "http://localhost:3000/battles"
+
+        fetch(url, {
+            headers: {
+               Authorization: auth
+             } 
+        })
+        .then(response => response.json())
+        .then(data => this.setState({ team:data }));
+        
+    }
+ */
 
         render(){
             return(
-                <div>ALL BATTLES</div>
+                <div className="text-center">
+            <img className="cover" src="https://www.searchpng.com/wp-content/uploads/2019/02/Avengers-Logo-PNG-Image-715x302.png" />
+           
+           <div className="container">
+                
+                <div className="row justify-content-center" >
+                    
+                    <div className="col-md-4">
+                        <b>History of all your battles </b>
+                        {this.state.battles.map(battles =>(
+                            
+                            <ul className="list-group">
+                            <Link to={'/battle/' + battles.id}  className="list-group-item list-group-item-action list-group-item-light" > 
+                            Winners : {battles.winteam}  </Link>
+                            </ul>
+                             ))}
+
+                    <i>Click on the battle to view the details</i>
+                    </div>          
+
+                </div>
+           
+               
+                </div>
+            
+            </div>
             )
         }
-}
+}   
 
 export  default BattleHistory
